433cf7f0bc0b8384adef52fe8fc95020927bafe0
Mobin Veisy <mobinveisy@yahoo.com>
2022-07-11 01:25:35 +0430

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8b052e741f41b861"; }
+/******/ 		__webpack_require__.h = function() { return "6e7133a43bb0e629"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
